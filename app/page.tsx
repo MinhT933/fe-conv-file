58b99cd4d8fd92b5e190aad294b7c669cd23c705
toanpm933 <toanpham@pitel.vn>
2025-10-21 09:45:49 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div className="w-full flex justify-center items-center bg-pink-100">
+      <div className="w-full flex text-black justify-center items-center bg-pink-100">
         Lạy chúa con theo đạo phật
       </div>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
